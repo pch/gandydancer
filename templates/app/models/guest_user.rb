fff06b08e8a8d822f2902877ab64f9a9282d8973
Piotr Chmolowski <piotr@chmolowski.pl>
2017-09-28 19:58:10 +0200

    Remove this

--- templates/app/models/guest_user.rb
+++ templates/app/models/guest_user.rb
@@ -10,24 +10,4 @@ class GuestUser
   def admin?
     false
   end
-
-  def followed_by?(user)
-    false
-  end
-
-  def wants?(product)
-    false
-  end
-
-  def owns?(product)
-    false
-  end
-
-  def mehs?(product)
-    false
-  end
-
-  def loves?(product)
-    false
-  end
 end
